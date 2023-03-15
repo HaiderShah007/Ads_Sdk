54b7f01bf05f0567825f883c756dd0053fff80f1
haider shah <example@gmail.com>
2023-03-15 18:01:26 +0500

    SDK Implemented

---
+++ AdsSDK/src/main/java/com/example/adssdk/BannerAds.kt
@@ -0,0 +1,11 @@
+package com.example.adssdk
+
+import android.app.Activity
+import android.view.View
+import com.google.android.gms.ads.AdSize
+
+class BannerAds(activity: Activity, layout:View) {
+    init {
+        AdSize.BANNER
+    }
+}
--- app/src/main/java/com/example/createcustomlibrary/MainActivity.kt
+++ app/src/main/java/com/example/createcustomlibrary/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.createcustomlibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.adssdk.BannerAds
 import com.example.adssdk.ToasterMessage
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         ToasterMessage.s(this,"Hello")
+        BannerAds(activity = this, layout = findViewById(R.id.text))
     }
 }
